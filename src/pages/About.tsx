2315348efa728cf48e8c4f584c329734bad5d14e
kriteshTimsina <kriteshtimsina55@gmail.com>
2024-04-02 07:12:03 +0545

    refactor: about us

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,6 @@
 import { RxInfoCircled } from "react-icons/rx";
 import CallToAction from "../components/CallToAction";
+
 const About = () => {
   return (
     <div className="flex flex-col mt-10 gap-6">
@@ -9,8 +10,8 @@ const About = () => {
       </section>
       <section className="px-5 text-typography">
         <p className="mb-3 selection:bg-yellow-300">
-        I'm a highly motivated computer science enthusiast with a passion for problem-solving and software development. I quickly adapt to new challenges and opportunities to broaden my skills. With strong analytical abilities and a keen eye for detail, I believe in my capability to contribute effectively to any team, ensuring success in my career as a software developer.
-        When I'm not coding, you can find me strumming my guitar and crafting songs—a creative outlet that complements my technical pursuits perfectly. Join me as I explore the fascinating worlds and music!
+        I'm a highly motivated computer science enthusiast with a passion for problem-solving and software development. With strong analytical abilities and a keen eye for detail, I believe in my capability to contribute effectively to any team, ensuring success in my career as a software developer.
+        When I'm not coding, you can find me strumming my guitar and crafting songs. Join me as I explore the fascinating worlds of technology and music!
         </p>
         <CallToAction
           href="https://www.youtube.com/@kritstims"
