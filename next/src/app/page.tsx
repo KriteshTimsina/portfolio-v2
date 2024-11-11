bcf86bd8b2b8c4c057b25227c2841d391ed798eb
Kritesh Timsina <kriteshtimsina55@gmail.com>
2024-11-11 08:33:59 +0545

    feat: create meta tags

--- next/src/app/page.tsx
+++ next/src/app/page.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
 import React from "react";
-
+import logo from "/public/kritesh-timsina.jpg";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Image alt="S" height={100} width={200} src={logo} />
+    </div>
+  );
 };
 
 export default Home;
